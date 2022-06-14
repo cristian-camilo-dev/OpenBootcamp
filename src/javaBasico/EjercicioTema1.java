c2de68d1aaee91fa12e5d31f1d0b76b3dd469128
cristianrg149 <cristianrodrig149@gmail.com>
2022-06-14 15:41:08 +0200

    Ejercicio tema 1 java basico v2

--- src/javaBasico/EjercicioTema1.java
+++ src/javaBasico/EjercicioTema1.java
@@ -1,2 +1,32 @@
-package javaBasico;public class EjercicioTema1 {
+package javaBasico;
+
+public class EjercicioTema1 {
+    public static void main(String[] args) {
+
+        /*
+ Para este primer ejercicio tendréis que realizar lo siguiente:
+
+Crea un proyecto de Java desde 0
+
+Dentro del proyecto tenéis que crear un paquete. En el paquete tendréis que crear una clase.
+
+Dentro de la clase tenéis que crear el método main e imprimir todos los datos que se han visto en las sesiones.
+
+Recordatorio: Los tipos de datos más comunes son int, long, double, boolean, String.
+        */
+
+        int numero = 10;
+        long numeroLong = 10;
+        double numeroDouble = 10.0;
+        boolean numeroBoolean = true;
+        String numeroString = "5";
+
+        System.out.println("Numero: " + numero);
+        System.out.println("Numero Long: " + numeroLong);
+        System.out.println("Numero Double: " + numeroDouble);
+        System.out.println("Numero Boolean: " + numeroBoolean);
+        System.out.println("Numero String: " + numeroString);
+
+
+    }
 }
